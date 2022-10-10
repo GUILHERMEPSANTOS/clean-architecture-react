da3f8ab14945ef49d75f1a9888382c0a62889952
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-10-09 21:54:33 -0300

    refactor: move SUT

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -5,10 +5,12 @@ import { AxiosHttpClient } from "./axios-http-client"
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
+const makeSut = (): AxiosHttpClient  =>  new AxiosHttpClient()
+
 describe('AxiosHttpClient', () => {
     test('Should call axios with correct URL',async  () => {
+        const sut = makeSut()
         const url = faker.internet.url()
-        const sut = new AxiosHttpClient()
         await sut.post({ url })
         expect(mockedAxios).toHaveBeenCalledWith(url)
     })
