fcb5243d9e1e00e0a1c74525fa5f972d59cf9474
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-10-09 22:13:51 -0300

    feat: correct values in axios

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,17 +1,22 @@
 import axios from 'axios'
 import { faker } from '@faker-js/faker'
 import { AxiosHttpClient } from "./axios-http-client"
+import { HttpPostParams } from '@/data/protocols/http'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
-const makeSut = (): AxiosHttpClient  =>  new AxiosHttpClient()
+const makeSut = (): AxiosHttpClient => new AxiosHttpClient()
 
+const mockPostRequest = (): HttpPostParams<any> => ({
+    url: faker.internet.url(),
+    body: {}
+})
 describe('AxiosHttpClient', () => {
-    test('Should call axios with correct URL and verb',async  () => {
+    test('Should call axios with correct values', async () => {
         const sut = makeSut()
-        const url = faker.internet.url()
-        await sut.post({ url })
-        expect(mockedAxios.post).toHaveBeenCalledWith(url)
+        const request = mockPostRequest()
+        await sut.post(request)
+        expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
     })
 })
