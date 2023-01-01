52cc2d805be23e1cdf3cb0bae2cad66be3dc8700
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-01 19:41:47 -0300

    feat: ensure remoteAuthentication returns an account on success

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,17 +1,18 @@
 import { HttpPostClient } from '@/data/protocols/http/http-post-client';
 import { HttpStatusCode } from '@/data/protocols/http/http-response';
+
+import { AccountModel } from '@/domain/models/account-model';
+import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication';
 import { InvalidCredentialError } from '@/domain/errors/invalid-credential-error';
 import { UnexpectedError } from '@/domain/errors/unexpected-error';
-import { AccountModel } from '@/domain/models/account-model';
-import { AuthenticationParams } from '@/domain/usecases/authentication';
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
     constructor(
         private readonly httpPostClient: HttpPostClient,
         private readonly url: string,
     ) { }
 
-    async auth(params?: AuthenticationParams): Promise<AccountModel> {
+    async auth(params: AuthenticationParams): Promise<AccountModel> {
         const httpResponse = await this.httpPostClient.post<AccountModel, AuthenticationParams>({ url: this.url, body: params });
 
         switch (httpResponse.status) {
@@ -29,8 +30,6 @@ export class RemoteAuthentication {
 
             default:
                 throw new UnexpectedError();
-
-
         }
     }
 }
--- src/domain/test/mock-authentication.ts
+++
@@ -1,8 +0,0 @@
-import { faker } from '@faker-js/faker';
-
-import { AuthenticationParams } from '@/domain/usecases/authentication';
-
-export const mockAuthentication = (): AuthenticationParams => ({
-    email: faker.name.firstName(),
-    senha: faker.animal.cat(),
-});
