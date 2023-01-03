5205360707f68a739970eadec9151844c211eada
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-03 13:29:08 -0300

    test: ensure axioshttpclient  call axios with correct url

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,32 @@
+import { AxiosHttpClient } from "./axios-http-client";
+
+import axios from 'axios';
+import { faker } from "@faker-js/faker";
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+type SutTypes = {
+    sut: AxiosHttpClient<any, any>
+}
+
+const mockSut = (): SutTypes => {
+    const sut = new AxiosHttpClient<any, any>();
+
+    return {
+        sut
+    }
+}
+
+describe("AxiosHttpClient", () => {
+    test("Should call axios with correct URL", async () => {
+        const { sut } = mockSut();
+        
+        const url = faker.internet.url();
+        const body = {};        
+        
+        await sut.post({ url: url, body: body });
+
+        expect(mockedAxios).toHaveBeenCalledWith(url);
+    });
+});
