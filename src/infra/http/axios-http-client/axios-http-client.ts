8301fd3d8a9c65921f24755f0fbfc78a14532363
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-03 13:28:54 -0300

    feat: ensure axioshttpclient  call axios with correct url

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,12 @@
+import { HttpPostClientParams } from '@/data/protocols/http';
+
+import axios from 'axios';
+
+export class AxiosHttpClient<Response, Params>
+// implements HttpPostClient<Response, Params>
+{
+    async post({ url, body }: HttpPostClientParams<Params>): Promise<void> {
+
+        await axios(url);
+    }
+}
