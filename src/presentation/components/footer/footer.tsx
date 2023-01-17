a31a192670365a2a2c8bbec4e004a3f653088dfb
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-17 11:25:10 -0300

    refactor: move header, footer and logo to components

---
+++ src/presentation/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Styles from "./footer-styles.scss";
+
+const Footer = () => {
+  return <footer className={Styles.footer}></footer>;
+};
+
+export default Footer;
