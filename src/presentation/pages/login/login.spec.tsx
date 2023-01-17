4af016314f4ca61a63c928beda118086aec736cc
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-17 13:13:05 -0300

    chore: add testing library

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Login from "./login";
+
+describe("Login Component", () => {
+  test("", () => {
+    render(<Login />);
+  });
+});
